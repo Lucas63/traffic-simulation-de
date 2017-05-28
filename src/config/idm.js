a4c32b5fa2319e9f4fdc7c87221459eadcad6675
Baphomet-- <al.chekanin@gmail.com>
2017-05-28 10:50:15 +0300

    Updated configuration for IDM model

--- src/config/idm.js
+++ src/config/idm.js
@@ -2,47 +2,88 @@ var idm_json =
 {
 	"car":
 	{
-		"desired_speed": 80,
-		"time_headway": 1.5,
 		"minimum_gap": 2,
 		"acceleration": 1.5,
 		"deceleration": 1.8,
+
+		"free_road":
+		{
+			"desired_speed": 80,
+			"time_headway": 1.5,
+			"lambda_T": 1,
+			"lambda_a": 1,
+			"lambda_b": 1,
+		},
+
+		"upstream":
+		{
+			"desired_speed": 20,
+			"time_headway": 1.6,
+			"lambda_T": 1,
+			"lambda_a": 1,
+			"lambda_b": 1.7,
+		},
+
+		"downstream":
+		{
+			"desired_speed": 80,
+			"time_headway": 1.5,
+			"lambda_T": 0.5,
+			"lambda_a": 1,
+			"lambda_b": 2,
+		},
+
+		"jam":
+		{
+			"desired_speed": 10,
+			"time_headway": 1.5,
+			"lambda_T": 0.5,
+			"lambda_a": 1.5,
+			"lambda_b": 1,
+		}
 	},
 
 	"truck":
 	{
-		"desired_speed": 60,
-		"time_headway": 1.8,
 		"minimum_gap": 2,
 		"acceleration": 1.2,
 		"deceleration": 1.5,
+
+		"free_road":
+		{
+			"desired_speed": 60,
+			"time_headway": 1.8,
+			"lambda_T": 1,
+			"lambda_a": 1,
+			"lambda_b": 1,
+		},
+
+		"upstream":
+		{
+			"desired_speed": 15,
+			"time_headway": 1.8,
+			"lambda_T": 1,
+			"lambda_a": 1,
+			"lambda_b": 1.7,
+		},
+
+		"downstream":
+		{
+			"desired_speed": 60,
+			"time_headway": 1.5,
+			"lambda_T": 0.5,
+			"lambda_a": 1,
+			"lambda_b": 2,
+		},
+
+		"jam":
+		{
+			"desired_speed": 10,
+			"time_headway": 1.5,
+			"lambda_T": 0.5,
+			"lambda_a": 1.5,
+			"lambda_b": 1,
+		}
 	},
 
-	"free_road":
-	{
-		"lambda_T": 1,
-		"lambda_a": 1,
-		"lambda_b": 1,
-	},
-
-	"upstream":
-	{
-		"lambda_T": 1,
-		"lambda_a": 1,
-		"lambda_b": 1.7,
-	},
-
-	"downstream":
-	{
-		"lambda_T": 0.5,
-		"lambda_a": 1,
-		"lambda_b": 2,
-	},
-
-	"jam":
-	{
-		"lambda_T": 0.5,
-		"lambda_a": 1.5,
-		"lambda_b": 1,
-	}
 };
