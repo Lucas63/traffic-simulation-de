98b43ea7253d8e14c5f8eef86d6c0b366e85ae7e
Baphomet-- <al.chekanin@gmail.com>
2017-06-02 00:42:46 +0300

    Added computation of Bezier curve length

--- src/utils/common.js
+++ src/utils/common.js
@@ -44,3 +44,48 @@ function printError( functionName, message )
 {
 	console.error(functionName + ": " + message);
 }
+
+// TODO move this code for appropriate place in renderer
+
+// just already computed fixed coefficients for calculating Bezier curve length
+const COEF1 = 5/9;
+const COEF2 = 8/9;
+const COEF3 = 1.774597;
+const COEF4 = 0.225403;
+
+// t - parameter for Bezier curve
+// x1,y1 - coordinates of start point
+// x2,y2 - cooridinates of control point
+// x3, y3 - coordinates of end point
+function getBezierCurveLength(t, x1, y1, x2, y2, x3, y3)
+{
+	// This formula taken from "Approximate Arc Length Parametrization"
+	// MARCELO WALTER and ALAIN FOURNIER. Formula 7
+	let b = t / 2;
+
+	let s = 0; // length of the curve
+
+	s = b * (COEF1 * getNormOfCurve(COEF3 * b, x1, y1, x2, y2, x3, y3) +
+			COEF2 * getNormOfCurve(b, x1, y1, x2, y2, x3, y3) +
+			COEF1 * getNormOfCurve(COEF4 * b, x1, y1, x2, y2, x3, y3));
+
+	return s;
+}
+
+function getBezierCurveX(t, x1, x2, x3)
+{
+	return Math.pow(1 - t, 2) * x1 + 2 * (1 - t) * x2 + Math.pow(t, 2) * x3;
+}
+
+function getBezierCurveY(t, y1, y2, y3)
+{
+	return Math.pow(1 - t, 2) * y1 + 2 * (1 - t) * y2 + Math.pow(t, 2) * y3;
+}
+
+function getNormOfCurve(t, x1, y1, x2, y2, x3, y3)
+{
+	let x = getBezierCurveX(t, x1, x2, x3);
+	let y = getBezierCurveY(t, y1, y2, y3);
+
+	return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+}
