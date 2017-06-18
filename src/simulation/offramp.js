f62b2b8d8de6a51ce3f967eb8904ffc931bdcf95
Artem Malinovskyi <lucas63@yandex.com>
2017-06-18 14:24:03 +0300

    @lucas63

--- src/simulation/offramp.js
+++ src/simulation/offramp.js
@@ -24,6 +24,7 @@ function Offramp( _source, _destination, _outflow,
 	this.backwardLanes = new Array( this.source.getBackwardLanesAmount() );
 	setupPassLanes(this.backwardLanes);
 
+
 	let turnSourceLane = null;
 	return;
 
