9603d370c84f3605cd09730a1ef5a665df30645f
Artem Malinovskyi <lucas63@yandex.com>
2017-06-20 06:26:20 +0300

    @lucas63 : uncommited animation

--- src/render/renderer.js
+++ src/render/renderer.js
@@ -5,21 +5,20 @@
 canvas = document.getElementById('canvas_map');
 context = document.getElementById('canvas_map').getContext('2d');
 
-function Renderer(_map){
-	this.map_object = _map;
+function Renderer(_map) {
+    this.map_object = _map;
 
-	this.map_color = "";
-	this.road_color = "";
-	this.dotted_line_color = "";
-	this.boiled_line_color = "";
-	this.road_side_line_color = "";
+    this.map_color = "";
+    this.road_color = "";
+    this.dotted_line_color = "";
+    this.boiled_line_color = "";
+    this.road_side_line_color = "";
 }
 
 
-
-Renderer.prototype.draw_map = function(){
-	render_map(this);
-	context.save();
+Renderer.prototype.draw_map = function () {
+    render_map(this);
+    context.save();
 };
 
 
@@ -30,85 +29,82 @@ var x = 2;
 Renderer.prototype.update_map = function () {
 
 
-	//context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-	//сontext.moveTo(0,0);
-	//this.draw_map();
-	//context.restore();
-
-
-	let on_roads = this.map_object.get_road_lanes_with_car();
-	let on_onramps = this.map_object.get_onramp_lanes_with_car();
-	let on_offramps = this.map_object.get_offramp_lanes_with_car();
-	let on_turns = this.map_object.get_turn_lanes_with_car();
-
-	console.log(on_roads);
-	// each road
-	if(on_roads == null)
-		return;
-	for( let i = 0; i < on_roads.length; i++){
-		let road = on_roads[i][1];
-		// each lane
-		console.log(road);
-		if(road.backwardLanes == null)
-			break;
-		for( let j = 0; j < road.backwardLanes.length; j++){
-
-			let lane = road.backwardLanes[j];
-            console.log(lane);
-			// each vehicle
-			for( let k = 0; k < lane.vehicles.length; k++){
-
-				let vehicle = lane.vehicles[k];
-				console.log(vehicle);
-				x +=2;
-				console.log(x);
-				let canvas_object = get_canvas_object(vehicle.type,x,10,45);
-				//draw_car(canvas_object);
-			}
-		}
+    //сontext.moveTo(0,0);
+    this.draw_map();
+    //context.restore();
 
 
-	}
+    let on_roads = this.map_object.get_road_lanes_with_car();
+    let on_onramps = this.map_object.get_onramp_lanes_with_car();
+    let on_offramps = this.map_object.get_offramp_lanes_with_car();
+    let on_turns = this.map_object.get_turn_lanes_with_car();
 
-	for( let i = 0; i < on_onramps.length; i++){
+    console.log(on_roads);
 
-	}
+    if (on_roads == null)
+        return;
 
-	for( let i = 0; i < on_offramps.length; i++){
+    // each lane
+    for (let i = 0; i < on_roads.length; i++) {
+        let lane = on_roads[i][1];
 
-	}
+        console.log(lane);
 
-	for( let i = 0; i < on_turns.length; i++){
+        // each vehicle
+        for (let k = 0; k < lane.vehicles.length; k++) {
 
-	}
+            let vehicle = lane.vehicles[k];
+            console.log(vehicle);
+            x += 0.2;
+            console.log(x);
+            let canvas_object = get_canvas_object(vehicle.type, x, 10, 45);
+            draw_car(canvas_object);
+        }
+    }
+
+
 };
+
+// for (let i = 0; i < on_onramps.length; i++) {
+//
+// }
+//
+// for (let i = 0; i < on_offramps.length; i++) {
+//
+// }
+//
+// for (let i = 0; i < on_turns.length; i++) {
+//
+// }
+// }
+// ;
 
 
 // ------- UTILS
 
 /*
 
-Creating canvas object for car
+ Creating canvas object for car
 
  */
-function get_canvas_object(type, spawnX, spawnY, angle)
-{
-	let img_string = 'sources/truck.ico';
+function get_canvas_object(type, spawnX, spawnY, angle) {
+    let img_string = 'sources/truck.ico';
 
-	if(type == 0)
-		img_string = 'sources/car.ico';
+    if (type == 0)
+        img_string = 'sources/car.ico';
 
-	let canvas_object = new Image();
+    let canvas_object = new Image();
 
-	canvas_object.src = img_string;
+    canvas_object.src = img_string;
 
-	canvas_object.X = spawnX;
-	canvas_object.Y = spawnY;
+    canvas_object.X = spawnX;
+    canvas_object.Y = spawnY;
 
-	canvas_object.width = 1;
-	canvas_object.height = 1.5;
-	canvas_object.angle = angle;
+    canvas_object.width = 1;
+    canvas_object.height = 1.5;
+    canvas_object.angle = angle;
 
-	return canvas_object;
+    return canvas_object;
 }
